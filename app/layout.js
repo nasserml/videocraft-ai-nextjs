4ff162b388400de1ee2a4ba6ab7f033d17ee4588
Mahmoud Nasser <mnasserone@gmail.com>
2024-12-13 13:57:53 +0200

    shadcn added

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
+
 
 export const metadata = {
   title: "Create Next App",
@@ -18,9 +11,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      
       >
         {children}
       </body>
